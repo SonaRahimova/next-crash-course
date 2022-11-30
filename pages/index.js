804a0e7830e1404cea324c8f4338291061d377d6
suay-selay <70511142+suay-selay@users.noreply.github.com>
2022-11-30 17:00:41 +0100

    Chore: added data and index

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({articles}) {
 export const getStaticProps = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
   const articles = await res.json()
-
+  console.log(articles)
   return {
     props: {
       articles

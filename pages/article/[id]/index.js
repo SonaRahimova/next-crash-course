8b0c6a86b8368ccacab0132adfac385bacd2208b
suay-selay <70511142+suay-selay@users.noreply.github.com>
2022-12-01 10:13:06 +0100

    Chore: config file and fetch data from out own api

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from '../../../config'
 import Link from 'next/link'
 import { useRouter } from "next/router"
 
@@ -16,6 +17,33 @@ const article = ({ article }) => {
 }
 
 export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/articles/${context.
+  params.id}`)
+
+  const article = await res.json()
+
+  return {
+      props: {
+        article,
+      },
+    }
+}
+
+export const getStaticPaths = async () => {
+      const res = await fetch(`${server}/api/articles`)
+  
+      const articles = await res.json()
+
+      const ids = articles.map((article) => article.id)
+      const paths = ids.map((id) => ({params: {id: id.toString() } }))
+
+      return {
+          paths,
+          fallback: false,
+      }
+  }
+
+/* export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.
     params.id}`)
 
@@ -40,7 +68,7 @@ export const getStaticPaths = async () => {
             paths,
             fallback: false,
         }
-    }
+    } */
 
 
 
